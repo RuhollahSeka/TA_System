7919fd6369ed22ed6ad7482ace665f93fb0c61b1
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-10-30 18:48:43 +0330

    Add score model

---
+++ users/models/score.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from users.models import Student, Subject
+
+
+class SubjectScore(models.Model):
+    user_profile = models.ForeignKey(
+        to=Student,
+        on_delete=models.CASCADE,
+        verbose_name='حساب کاریری'
+    )
+
+    subject = models.ForeignKey(
+        to=Subject,
+        on_delete=models.CASCADE,
+        verbose_name='درس پاس شده',
+    )
+
+    score = models.PositiveSmallIntegerField(
+        verbose_name='نمره درس',
+    )

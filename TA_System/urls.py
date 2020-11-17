96baebb3ec2b1e519c828b844d8bbfadd3625c97
rev <alinajim@comp.nus.edu.sg>
2020-11-18 01:39:05 +0330

    added url

--- TA_System/urls.py
+++ TA_System/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from TA_System.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # dashboard
+    path('', index)
 ]

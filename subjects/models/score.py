96ac581702f221a595e18d1e748d8b1f99eff86f
rev <alinajim@comp.nus.edu.sg>
2020-11-17 23:04:00 +0330

    fixed nim-fasele

--- subjects/models/score.py
+++ subjects/models/score.py
@@ -13,9 +13,9 @@ class SubjectScore(models.Model):
     subject = models.ForeignKey(
         to=Subject,
         on_delete=models.CASCADE,
-        verbose_name='درس پاس شده',
+        verbose_name='درس پاس‌شده',
     )
 
     score = models.PositiveSmallIntegerField(
-        verbose_name='نمره درس',
+        verbose_name='نمره‌ی درس',
     )

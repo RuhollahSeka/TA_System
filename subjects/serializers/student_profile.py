7df570d94f110794da62889052b604036811c6c2
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-19 08:54:15 +0330

    Add scores to student profile API

--- subjects/serializers/student_profile.py
+++ subjects/serializers/student_profile.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from subjects.models import Student
+from subjects.models import Student, CourseScore
 
 
 class StudentProfileSerializer(serializers.ModelSerializer):
@@ -8,6 +8,18 @@ class StudentProfileSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(source='user.last_name')
     username = serializers.CharField(source='user.username')
     email = serializers.CharField(source='user.email')
+    scores = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_scores(student: Student):
+        # TODO send more data in this field
+        course_scores = CourseScore.objects.filter(
+            student=student
+        ).select_related('course__subject').values('score', 'course__subject__name')
+
+        return {
+            course_score['course__subject__name']: course_score['score'] for course_score in course_scores
+        }
 
     class Meta:
         model = Student
@@ -22,10 +34,12 @@ class StudentProfileSerializer(serializers.ModelSerializer):
             'resume',
             'university',
             'student_id',
+            'scores',
         )
 
         read_only_fields = (
             'id',
             'university',
             'student_id',
+            'scores',
         )

9039d845217a7177841123219b28aa6bc6809168
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-19 08:12:14 +0330

    Add student profile serializer

---
+++ subjects/serializers/student_profile.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from subjects.models import Student
+
+
+class StudentProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    username = serializers.CharField(source='user.username')
+    email = serializers.CharField(source='user.email')
+
+    class Meta:
+        model = Student
+        fields = (
+            'first_name',
+            'last_name',
+            'username',  # TODO do we need this?
+            'email',
+            'phone_number',
+            'resume',
+            'university',
+            'student_id',
+        )
+
+        read_only_fields = (
+            'university',
+            'student_id',
+        )

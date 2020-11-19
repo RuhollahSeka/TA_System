02276398f8930e35f199f9c2fe57d87c83151478
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-19 09:07:44 +0330

    Add RoleRequest model

--- ta_procedures/models/__init__.py
+++ ta_procedures/models/__init__.py
@@ -1,3 +1,4 @@
 from .course import Course
 from .role import Role
 from .role_attachment import RoleAttachment
+from .role_request import RoleRequest
---
+++ ta_procedures/models/role_request.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from _helpers.models import TimeModel
+from subjects.models import Student
+from ta_procedures.models import Role
+
+
+class RoleRequest(TimeModel):
+    STATE_PENDING = 'pending'
+    STATE_ACCEPTED = 'accepted'
+    STATE_REJECTED = 'rejected'
+
+    STATE_CHOICES = (
+        (STATE_PENDING, 'در حال انتظار'),
+        (STATE_ACCEPTED, 'تایید شده'),
+        (STATE_REJECTED, 'رد شده'),
+    )
+
+    student = models.ForeignKey(
+        to=Student,
+        on_delete=models.CASCADE,
+        verbose_name='دانشجو',
+    )
+
+    role = models.ForeignKey(
+        to=Role,
+        on_delete=models.CASCADE,
+        verbose_name='وظیفه',
+    )
+
+    state = models.CharField(
+        max_length=8,
+        default=STATE_PENDING,
+        choices=STATE_CHOICES,
+        verbose_name='وضعیت درخواست',
+    )

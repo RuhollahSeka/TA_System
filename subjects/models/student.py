1444330033d781ed575fc1cd5e0a77eca5716365
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-19 19:42:02 +0330

    Add scores and roles property to student model

--- subjects/models/student.py
+++ subjects/models/student.py
@@ -36,3 +36,37 @@ class Student(TimeModel):
         validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
         verbose_name='فایل رزومه استاد',
     )
+
+    average_score = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='معدل',
+    )
+
+    @property
+    def scores(self):
+        from subjects.models import CourseScore
+        course_scores = CourseScore.objects.filter(
+            student=self,
+        ).extra(
+            select={
+                'course__subject__name': 'name',
+            }
+        ).select_related('course__subject').values('score', 'course__subject__name', 'course_id')
+
+        return course_scores
+
+    @property
+    def roles(self):
+        from ta_procedures.models import Role
+        roles = Role.objects.filter(
+            student=self
+        ).extra(
+            select={
+                'name': 'role_name',
+                'course__subject__name': 'course_name',
+                'id': 'role_id',
+            }
+        ).select_related('course__subject').values('name', 'course__subject__name', 'course_id', 'id')
+
+        return roles

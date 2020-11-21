ec19b92e67a2b34147ed041d25addaaeea4d0187
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-22 01:17:00 +0330

    Add CourseAttachment model

--- ta_procedures/models/__init__.py
+++ ta_procedures/models/__init__.py
@@ -2,3 +2,4 @@ from .course import Course
 from .role import Role
 from .role_attachment import RoleAttachment
 from .role_request import RoleRequest
+from .course_attachment import CourseAttachment
---
+++ ta_procedures/models/course_attachment.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from _helpers.models import TimeModel
+from ta_procedures.models import Course
+
+
+class CourseAttachment(TimeModel):
+    course = models.ForeignKey(
+        to=Course,
+        on_delete=models.CASCADE,
+        related_name='attachments',
+        verbose_name='درس',
+    )
+
+    attachment = models.FileField(
+        upload_to='courses/',
+        verbose_name='فایل ضمیمه درس',
+    )

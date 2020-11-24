0269a29b6decfa9724ef26f864ebef4496b777d4
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-25 01:23:39 +0330

    Add urls

--- subjects/urls.py
+++ subjects/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from subjects.views import student_views, lecturer_views
 
 urlpatterns = [
+    # Student urls
     path('student-profile/', student_views.StudentProfileAPIView.as_view()),
+    path('lecturers/', student_views.LecturerAPIView.as_view()),
+    path('universities/', student_views.UniversityAPIView.as_view()),
+    path('subjects/', student_views.SubjectAPIView.as_view()),
+
     path('lecturer-profile/', lecturer_views.LecturerProfileAPIView.as_view()),
+    path('students/', lecturer_views.StudentViewSet, name='students')
 ]

dfd46c7735fa6e973f29bc75ac8dc0171fcd1e88
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-25 02:37:43 +0330

    Add ta_procedures app urls

--- ta_procedures/urls.py
+++ ta_procedures/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from TA_System.views import index
+from ta_procedures.views import admin_views, lecturer_views, student_views
 
 urlpatterns = [
+    # Lecturer views
+    path('lecturers/courses/', lecturer_views.CourseViewSet, name='lecturer-course-v1'),
+    path('lecturers/roles/', lecturer_views.RoleViewSet, name='lecturer-role-v1'),
+    path('lecturers/role-requests/', lecturer_views.RoleRequestViewSet, name='lecturer-role-request-v1'),
 
+    # Student views
+    path('students/courses/', student_views.CourseReadOnlyViewSet, name='student-course-v1'),
+    path('students/roles/', student_views.RoleReadOnlyViewSet, name='student-role-v1'),
+    path('students/role-requests/', student_views.RoleRequestViewSet, name='student-role-request-v1'),
 ]

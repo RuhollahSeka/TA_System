62e014a05d06b85eef273f68e24f373e8bfb7695
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-26 17:02:15 +0330

    Add recommendation model

--- ta_procedures/models/__init__.py
+++ ta_procedures/models/__init__.py
@@ -3,3 +3,4 @@ from .role import Role
 from .role_attachment import RoleAttachment
 from .role_request import RoleRequest
 from .course_attachment import CourseAttachment
+from .recommendation import Recommendation
---
+++ ta_procedures/models/recommendation.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from _helpers.models import TimeModel
+from subjects.models import Student
+from ta_procedures.models import Course
+
+
+class Recommendation(TimeModel):
+    student = models.ForeignKey(
+        to=Student,
+        on_delete=models.CASCADE,
+        related_name='recommendations',
+        verbose_name='دانشجو',
+    )
+
+    course = models.ForeignKey(
+        to=Course,
+        on_delete=models.CASCADE,
+        verbose_name='درس',
+    )
+
+    text = models.TextField(
+        verbose_name='متن',
+    )

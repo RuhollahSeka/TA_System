94591af2f15bb90bd8cbf940ccb433a9db3818a9
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-27 21:24:47 +0330

    Fix lecturer field in role filter

--- ta_procedures/filters/role.py
+++ ta_procedures/filters/role.py
@@ -9,15 +9,17 @@ class RoleFilter(django_filters.FilterSet):
     course_id__contains = django_filters.CharFilter(field_name='course__course_id', lookup_expr='contains')
     course_id__startswith = django_filters.CharFilter(field_name='course__course_id', lookup_expr='startswith')
 
-    lecturer_name = django_filters.CharFilter(field_name='lecturer__name')
-    lecturer_name__contains = django_filters.CharFilter(field_name='lecturer__name', lookup_expr='contains')
-    lecturer_name__startswith = django_filters.CharFilter(field_name='lecturer__name', lookup_expr='startswith')
+    lecturer_name = django_filters.CharFilter(field_name='course__lecturer__name')
+    lecturer_name__contains = django_filters.CharFilter(field_name='course__lecturer__name', lookup_expr='contains')
+    lecturer_name__startswith = django_filters.CharFilter(field_name='course__lecturer__name', lookup_expr='startswith')
+
+    lecturer = django_filters.NumberFilter(field_name='course__lecturer_id')
+    lecturer__in = django_filters.BaseInFilter(field_name='course__lecturer_id')
 
     class Meta:
         model = Role
         fields = {
             'id': ['exact', 'in'],
             'course': ['exact', 'in'],
-            'lecturer': ['exact', 'in'],
             'student': ['isnull'],
         }

56f56321055701854804d32c1dc7b8731094bded
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-27 06:21:26 +0330

    Add STATIC_ROOT setting

--- TA_System/settings.py
+++ TA_System/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config, Csv
 csv = Csv()
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (

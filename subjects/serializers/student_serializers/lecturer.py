c15874a6c3b207a0e9b13ffccc12a3f5d8e03864
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-11-28 04:55:38 +0330

    Fix subjects app doc

--- subjects/serializers/student_serializers/lecturer.py
+++ subjects/serializers/student_serializers/lecturer.py
@@ -4,8 +4,8 @@ from subjects.models import Lecturer
 
 
 class LecturerSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(source='user.first_name')
-    last_name = serializers.CharField(source='user.last_name')
+    first_name = serializers.CharField(source='user.first_name', read_only=True, label='نام استاد')
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
 
     class Meta:
         model = Lecturer

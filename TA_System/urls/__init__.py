c1f969c69e343570361873515fd17b00f8767fb9
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-12-18 16:01:52 +0330

    Fix admin url

--- TA_System/urls/__init__.py
+++ TA_System/urls/__init__.py
@@ -4,9 +4,7 @@ from django.urls import path, include
 from TA_System.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/v1/', include('TA_System.urls.v1_urls.api_urls')),
     path('docs/v1/', include('TA_System.urls.v1_urls.doc_urls')),
-    # dashboard
-    path('', index)
 ]
